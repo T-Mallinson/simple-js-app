50e1832a0d43b97746d2d8d23af9d4373b8057f6
T-Mallinson <117472781+T-Mallinson@users.noreply.github.com>
2022-11-14 16:51:39 -0600

    set alert and favouriteFood variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,4 @@
-let myName = 'Bob';
-document.write(myName);
+alert('Hello World');
 
-let simpleAddition = 2 + 2;
-document.write(simpleAddition);
-
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize *2) - (size / 2);
-document.write(minSize);
-
+var favouriteFood = 'Pizza';
+document.write(favouriteFood);
