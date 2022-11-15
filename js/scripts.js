6fb66a7e320061b645be5cb45bfa42e8f8ed9e26
T-Mallinson <117472781+T-Mallinson@users.noreply.github.com>
2022-11-15 15:50:42 -0600

    Updated 'types' to all be arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 let pokemonList=[
     {name: 'bulbasaur', types: ['grass', 'poison'], height: 70},
-    {name: 'squirtle', types: 'water', height: 50},
-    {name: 'charmander', types: 'fire', height: 60},
+    {name: 'squirtle', types: ['water'], height: 50},
+    {name: 'charmander', types: ['fire'], height: 60},
     {name: 'pidgey', types: ['flying', 'normal'], height: 30},
-    {name: 'pikachu', types: 'electric', height: 40},
+    {name: 'pikachu', types: ['electric'], height: 40},
     {name: 'weedle', types: ['bug', 'poison'], height: 30},
 ]
