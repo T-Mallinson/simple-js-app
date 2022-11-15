de7a4760680a55af35150465bd1b6111291b223f
T-Mallinson <117472781+T-Mallinson@users.noreply.github.com>
2022-11-15 13:38:38 -0600

    Created pokemonList Array with initial 6 objects
    created pokemonList Array with 6 initial objects (Pokemon), listing 3 properties (name, types and height)

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-alert('Hello World');
-
-var favouriteFood = 'Pizza';
-document.write(favouriteFood);
+let pokemonList=[
+    {name: 'bulbasaur', types: ['grass', 'poison'], height: 70}
+    {name: 'squirtle', types: 'water', height: 50}
+    {name: 'charmander', types: 'fire', height: 60}
+    {name: 'pidgey', types: ['flying', 'normal'], height: 30}
+    {name: 'pikachu', types: 'electric', height: 40}
+    {name: 'weedle', types: ['bug', 'poison'], height: 30}
+]
