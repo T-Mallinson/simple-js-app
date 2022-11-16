142308017f0dcad63237366fd2ef659e5c683291
T-Mallinson <117472781+T-Mallinson@users.noreply.github.com>
2022-11-16 16:49:19 -0600

    renamed pokemon with capitals

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 let pokemonList=[
-    {name: 'bulbasaur', types: ['grass', 'poison'], height: 70},
-    {name: 'squirtle', types: ['water'], height: 50},
-    {name: 'charmander', types: ['fire'], height: 60},
-    {name: 'pidgey', types: ['flying', 'normal'], height: 30},
-    {name: 'pikachu', types: ['electric'], height: 40},
-    {name: 'weedle', types: ['bug', 'poison'], height: 30},
+    {name: 'Bulbasaur', types: ['grass', 'poison'], height: 70},
+    {name: 'Squirtle', types: ['water'], height: 50},
+    {name: 'Charmander', types: ['fire'], height: 60},
+    {name: 'Pidgey', types: ['flying', 'normal'], height: 30},
+    {name: 'Pikachu', types: ['electric'], height: 40},
+    {name: 'Weedle', types: ['bug', 'poison'], height: 30},
 ]
 
 for (let i=0; i< pokemonList.length; i++) {
