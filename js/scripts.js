6192e87d623bce17f6db0ee86977a4ff8ee05924
T-Mallinson <117472781+T-Mallinson@users.noreply.github.com>
2022-11-17 10:12:14 -0600

    updated sheet styling and added breaks to list

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,38 @@
 let pokemonList=[
-    {name: 'Bulbasaur', types: ['grass', 'poison'], height: 70},
-    {name: 'Squirtle', types: ['water'], height: 50},
-    {name: 'Charmander', types: ['fire'], height: 60},
-    {name: 'Pidgey', types: ['flying', 'normal'], height: 30},
-    {name: 'Pikachu', types: ['electric'], height: 40},
-    {name: 'Weedle', types: ['bug', 'poison'], height: 30},
+    {name: 'Bulbasaur', 
+    types: ['grass', 'poison'], 
+    height: 70
+    },
+    {name: 'Squirtle',
+     types: ['water'],
+     height: 50
+    },
+    {name: 'Charmander', 
+    types: ['fire'], 
+    height: 60
+    },
+    {name: 'Pidgey', 
+    types: ['flying', 'normal'], 
+    height: 30
+    },
+    {name: 'Pikachu', 
+    types: ['electric'], 
+    height: 40
+    },
+    {name: 'Weedle', 
+    types: ['bug', 'poison'], 
+    height: 30
+    },
 ]
 
 for (let i=0; i< pokemonList.length; i++) {
     if (pokemonList[i].height < 40) {
-        document.write(pokemonList[i].name + " (height:  ", + pokemonList[i].height + ") " + " - ...pretty small ;  ")
+        document.write(pokemonList[i].name + " (height:  ", + pokemonList[i].height + ") " + " - ...pretty small ; <br>")
     }
     else if (pokemonList[i].height > 50) {
-        document.write(pokemonList[i].name + " (height:  ", + pokemonList[i].height + ") " + " - wow... ;      " )
+        document.write(pokemonList[i].name + " (height:  ", + pokemonList[i].height + ") " + " - wow... ;    <br>" )
     }
     else {
-        document.write(pokemonList[i].name + " (height:  ", + pokemonList[i].height + ") " + " - meh average ;     ")
+        document.write(pokemonList[i].name + " (height:  ", + pokemonList[i].height + ") " + " - meh average ;  <br>")
     }
 }
