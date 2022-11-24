f1e18c7320e18880f2964539b3a874f003ecb099
T-Mallinson <117472781+T-Mallinson@users.noreply.github.com>
2022-11-24 12:10:29 -0600

    wrapped pokemonList in IIFE - pokeomonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,45 @@
-let pokemonList=[
-    {name: 'Bulbasaur', 
-    types: ['grass', 'poison'], 
-    height: 70
-    },
-    {name: 'Squirtle',
-     types: ['water'],
-     height: 50
-    },
-    {name: 'Charmander', 
-    types: ['fire'], 
-    height: 60
-    },
-    {name: 'Pidgey', 
-    types: ['flying', 'normal'], 
-    height: 30
-    },
-    {name: 'Pikachu', 
-    types: ['electric'], 
-    height: 40
-    },
-    {name: 'Weedle', 
-    types: ['bug', 'poison'], 
-    height: 30
-    },
-]
+let pokemonRepository = (function () {
+    let pokemonList=[
+        {name: 'Bulbasaur', 
+        types: ['grass', 'poison'], 
+        height: 70
+        },
+        {name: 'Squirtle',
+         types: ['water'],
+         height: 50
+        },
+        {name: 'Charmander', 
+        types: ['fire'], 
+        height: 60
+        },
+        {name: 'Pidgey', 
+        types: ['flying', 'normal'], 
+        height: 30
+        },
+        {name: 'Pikachu', 
+        types: ['electric'], 
+        height: 40
+        },
+        {name: 'Weedle', 
+        types: ['bug', 'poison'], 
+        height: 30
+        },
+    ]
+
+    function getAll () {
+        return pokemonList;
+    }
+    function add (pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    }
+})()
+
+
 
 for (let i=0; i< pokemonList.length; i++) {
     if (pokemonList[i].height < 40) {
@@ -36,3 +52,11 @@ for (let i=0; i< pokemonList.length; i++) {
         document.write("<p>" + pokemonList[i].name + " (height:  ", + pokemonList[i].height + ") " + " - meh average ;" + "</p>")
     }
 }
+
+function printArrayDetails(list){
+    for (let i=0; i < list.length; i++){
+        document.write("<p>" + list[i].name + "</p>");
+    };
+};
+
+printArrayDetails(pokemonList);
